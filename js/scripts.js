75cad3b8ae4091d681cb7eed2c974a89c9d6ea68
Slawomir S <slaweks48@gmail.com>
2017-07-29 01:14:18 +0200

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,51 @@
+$(function() {
+    var carouselList = $("#myCarousel ul"),
+        carousel = $("#myCarousel"),
+        leftControl = $(".left-control"),
+        rightControl = $(".right-control"),
+        width = $('.slide').width(),
+        btnCircle = $(".btn-circle");
+    
+    function changeSlide() {
+        carouselList
+            .animate({marginLeft:-400}, 500, placeFirstSlide);
+    }    
+    
+    function interval() {
+        interval = setInterval(changeSlide,5000);
+    }
+    
+    function stopInterval () {
+		clearInterval(interval);
+    }
+    
+    carousel.on('mouseenter', stopInterval).on('mouseleave', interval);
+    interval();
+    
+    function placeFirstSlide () {
+        var firstItem = carouselList.find("li:first"),
+            lastItem = carouselList.find("li:last");
+        lastItem.after(firstItem);
+        carouselList.css({marginLeft:0});
+    }
+
+    function placeLastSlide() {
+		var firstItem = carouselList.find("li:first");
+		var lastItem = carouselList.find("li:last");
+		firstItem.before(lastItem);
+		carouselList.css({marginLeft:0});
+    }
+    
+    leftControl.click(function() {
+		carouselList.animate({'marginLeft': +400}, 500, placeLastSlide);
+	});
+	rightControl.click(function() {
+		carouselList.animate({'marginLeft':-400}, 500, placeFirstSlide);
+	});
+	btnCircle.on('click', function() {
+		var indicatorValue = $(this).data('indicator'),
+		    slideSelector = '.slide[data-slide="' + indicatorValue + '"',
+		    activeSlide = $(slideSelector);
+		carouselList.animate({'marginLeft': "-" + ((indicatorValue)*width) + 'px'}, 500);
+	});
+});
